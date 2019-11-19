6cbd0710fba76c3db6e32089a1dc734e54322bd6
divaymohan <divaymohan05@gmail.com>
2019-11-19 23:48:00 +0530

    updated condition

--- BST_Max_element_in_path_between_two_nodes/binaryMaxTree.c
+++ BST_Max_element_in_path_between_two_nodes/binaryMaxTree.c
@@ -1,95 +1,106 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct Node{
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
     int data;
-    struct Node *left,*right;
+    struct Node *left, *right;
 };
-struct Node* root;
-struct Node* createNode(int data){
-    struct Node *ReNode = (struct Node*)malloc(sizeof(struct Node));
+struct Node *root;
+struct Node *createNode(int data)
+{
+    struct Node *ReNode = (struct Node *)malloc(sizeof(struct Node));
     ReNode->data = data;
     ReNode->left = NULL;
     ReNode->right = NULL;
     return ReNode;
 }
-void insertBinarySearchTree(int data){
+void insertBinarySearchTree(int data)
+{
     struct Node *temp = root;
-    if(temp==NULL){
+    if (temp == NULL)
+    {
         root = createNode(data);
         return;
     }
     struct Node *preNode = temp;
-    while(temp!=NULL){
-        if(temp->data > data){
+    while (temp != NULL)
+    {
+        if (temp->data > data)
+        {
             preNode = temp;
-            temp = temp->left; 
+            temp = temp->left;
         }
-        else{
+        else
+        {
             preNode = temp;
             temp = temp->right;
         }
     }
-    if(preNode->data > data){
+    if (preNode->data > data)
+    {
         preNode->left = createNode(data);
     }
     else
     {
         preNode->right = createNode(data);
     }
-   
-    
-
 }
-void inorder(struct Node *rooot){
-    if(rooot!=NULL){
+void inorder(struct Node *rooot)
+{
+    if (rooot != NULL)
+    {
         inorder(rooot->left);
-        printf("%d ",rooot->data);
+        printf("%d ", rooot->data);
         inorder(rooot->right);
     }
-
 }
-int maxElement(int first,int last){
+int maxElement(int first, int last)
+{
     struct Node *temp = root;
-    while(temp){
-        if(temp->data > first && temp->data < last){
+    while (temp)
+    {
+        if (temp->data > first && temp->data < last)
+        {
             break;
         }
-        else if(temp->data > first && temp->data > last){
+        else if (temp->data > first && temp->data > last)
+        {
             temp = temp->left;
-
         }
-        else{
+        else
+        {
             temp = temp->right;
         }
     }
-    while(temp->right!= NULL && temp->left->data != last){
+    while (temp->data < last)
+    {
         temp = temp->right;
     }
     return temp->data;
-
 }
-int main(int argc,char *argv[]){
+int main(int argc, char *argv[])
+{
     FILE *fp;
-    fp = fopen(argv[1],"r");
+    fp = fopen(argv[1], "r");
     int n;
-    fscanf(fp,"%d",&n);
-    root=NULL;
+    fscanf(fp, "%d", &n);
+    root = NULL;
     //struct Node *temp =NULL;
-    struct Node *trav =NULL;
+    struct Node *trav = NULL;
     int temp;
     int count = 0;
-    while(count<n){
-        fscanf(fp,"%d ",&temp);
+    while (count < n)
+    {
+        fscanf(fp, "%d ", &temp);
         insertBinarySearchTree(temp);
         count++;
-
     }
-   // printf("%d",root->data);
+    // printf("%d",root->data);
     inorder(root);
-    int first,last;
-    fscanf(fp,"%d",&first);
-    fscanf(fp,"%d",&last);
-    int max = maxElement(first,last);
-    printf("\nMaximum element :: %d\n",max);
+    int first, last;
+    fscanf(fp, "%d", &first);
+    fscanf(fp, "%d", &last);
+    int max = maxElement(first, last);
+    printf("\nMaximum element :: %d\n", max);
     return 0;
 }
